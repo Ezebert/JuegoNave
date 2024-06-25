b37b1fb1778365ad33616d6574ea29f774933691
Ezequiel Bertinat <eze.bertinat@gmail.com>
2024-06-25 12:50:34 -0300

    Primer Juego SFML Uso de RectangleShape

---
+++ SFML_3/main_2.cpp
@@ -0,0 +1,16 @@
+#include <SFML/Graphics.hpp>
+#include <Windows.h>
+#include <iostream>
+//ramddom
+#include <ctime>
+#include <stdlib.h>
+//Archivos .h
+#include "gamePlay.h"
+
+using namespace std;
+static int main_2()
+{
+    cout << "Hola mundo";
+
+    return 0;
+}
